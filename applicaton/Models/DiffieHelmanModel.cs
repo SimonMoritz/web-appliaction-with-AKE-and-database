4ed095d2988b9fdaf822b0e32aa49e481b735528
Simon Moritz <simon.moritz@protonmail.com>
2023-08-16 01:24:38 +0200

    begun key exchange implementation and test

---
+++ applicaton/Models/DiffieHelmanModel.cs
@@ -0,0 +1,10 @@
+namespace applicaton.Models;
+
+public class DiffieHelmanModel
+{
+    public int Generator { get; set; }
+
+    public int Summ(int a, int b){
+        return a + b;
+    }
+}
--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -8,5 +8,12 @@ public class UnitTest1
     {
         ErrorViewModel e = new();
         Assert.Equal(5, e.Summ(2,3));
+    }
+
+    [Fact]
+    public void DHmodelShouldGiveReasonableGenerator(){
+        DiffieHelmanModel dh = new();
+        dh.Generator = 100;
+        Assert.Equal(100, dh.Generator);
     }    
 }
